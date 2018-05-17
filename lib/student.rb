374e41ec32a8ed3043cc95aa089dbd8c5dd1f0f8
Eddie Moon <eddiemoonie@gmail.com>
2018-05-17 19:10:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -109,4 +109,17 @@ class Student
       self.new_from_db(row)
     end.first
   end
+
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
 end
